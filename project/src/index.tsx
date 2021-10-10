2cf7bdec1641615aa5a0792491050cdcc2e06606
sorokka <sorokka@MacBook-Pro-sorokka.local>
2021-10-10 13:40:32 +0300

    Разобьет на компоненты

--- project/src/index.tsx
+++ project/src/index.tsx
@@ -2,8 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/app/app';
 
+const mainPageValues = {
+  PLACES_COUNT: 19,
+  PLACES_CARDS: [1, 2, 3, 4, 5],
+};
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App placesCount = {mainPageValues.PLACES_COUNT} placesCards = {mainPageValues.PLACES_CARDS}/>
   </React.StrictMode>,
   document.getElementById('root'));
